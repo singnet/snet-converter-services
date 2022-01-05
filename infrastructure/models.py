fb2140d1cc100a4cd3b5ba28392245a3b4e32e40
karthi96 <karthikeyandon105@gmail.com>
2022-01-05 09:29:45 +0530

    adding auto increment id to all tables and adding guid column in few tables

--- infrastructure/models.py
+++ infrastructure/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, VARCHAR, TIMESTAMP, INTEGER, ForeignKey, UniqueConstraint, DECIMAL, BOOLEAN, BIGINT
+from sqlalchemy import Column, VARCHAR, TIMESTAMP, INTEGER, ForeignKey, UniqueConstraint, DECIMAL, BOOLEAN, BIGINT, \
+    func, text, TEXT
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -6,13 +7,16 @@ Base = declarative_base()
 
 
 class AuditClass(object):
-    created_at = Column("created_at", TIMESTAMP, nullable=False)
-    updated_at = Column("updated_at", TIMESTAMP, nullable=False)
+    id = Column("row_id", BIGINT, primary_key=True, autoincrement=True)
+    created_at = Column("created_at", TIMESTAMP(),
+                        server_default=func.current_timestamp(), nullable=False)
+    updated_at = Column("updated_at", TIMESTAMP(),
+                        server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
+                        nullable=False)
 
 
 class BlockChainDetailDBModel(Base, AuditClass):
     __tablename__ = "blockchain_detail"
-    id = Column("id", INTEGER, autoincrement=True, primary_key=True)
     chain_id = Column("chain_id", VARCHAR(50), nullable=False)
     name = Column("name", VARCHAR(50), nullable=False)
     symbol = Column("symbol", VARCHAR(30), nullable=False)
@@ -23,33 +27,30 @@ class BlockChainDetailDBModel(Base, AuditClass):
 
 class TokenDetailDBModel(Base, AuditClass):
     __tablename__ = "token_detail"
-    id = Column("id", INTEGER, autoincrement=True, primary_key=True)
     name = Column("name", VARCHAR(50), nullable=False)
-    description = Column("description", VARCHAR(250), nullable=False)
+    description = Column("description", TEXT, nullable=False)
     symbol = Column("symbol", VARCHAR(30), nullable=False)
     logo = Column("logo", VARCHAR(250))
-    blockchain_id = Column("blockchain_id", INTEGER, ForeignKey(BlockChainDetailDBModel.id), nullable=False)
+    blockchain_id = Column("blockchain_id", BIGINT, ForeignKey(BlockChainDetailDBModel.id), nullable=False)
     allowed_decimal = Column("allowed_decimal", INTEGER)
-    blockchain_detail = relationship('BlockChainDetailDBModel', uselist=False, lazy="select")
+    blockchain_detail = relationship(BlockChainDetailDBModel, uselist=False, lazy="select")
     __table_args__ = (UniqueConstraint(blockchain_id, symbol), {})
 
 
 class ConversionFeeDBModel(Base, AuditClass):
     __tablename__ = "conversion_fee"
-    id = Column("id", VARCHAR(250), primary_key=True)
     percentage_from_source = Column("percentage_from_source", DECIMAL)
     amount = Column("amount", BIGINT)
-    token_id = Column("token_id", INTEGER, ForeignKey(TokenDetailDBModel.id))
-    token_detail = relationship("TokenDetailDBModel", foreign_keys=[token_id])
-    __table_args__ = (UniqueConstraint(id), {})
+    token_id = Column("token_id", BIGINT, ForeignKey(TokenDetailDBModel.id))
+    token_detail = relationship(TokenDetailDBModel, foreign_keys=[token_id])
+    __table_args__ = (UniqueConstraint(AuditClass.id), {})
 
 
 class TokenPairDBModel(Base, AuditClass):
     __tablename__ = "token_pair"
-    id = Column("id", INTEGER, autoincrement=True, primary_key=True)
-    from_token_id = Column("from_token_id", INTEGER, ForeignKey(TokenDetailDBModel.id), nullable=False)
-    to_token_id = Column("to_token_id", INTEGER, ForeignKey(TokenDetailDBModel.id), nullable=False)
-    conversion_fee_id = Column("conversion_fee_id", VARCHAR(250), ForeignKey(ConversionFeeDBModel.id))
+    from_token_id = Column("from_token_id", BIGINT, ForeignKey(TokenDetailDBModel.id), nullable=False)
+    to_token_id = Column("to_token_id", BIGINT, ForeignKey(TokenDetailDBModel.id), nullable=False)
+    conversion_fee_id = Column("conversion_fee_id", BIGINT, ForeignKey(ConversionFeeDBModel.id))
     is_enabled = Column("is_enabled", BOOLEAN, default=True)
     min_value = Column("min_value", BIGINT)
     max_value = Column("max_value", BIGINT)
@@ -57,33 +58,34 @@ class TokenPairDBModel(Base, AuditClass):
     to_tn = relationship("TokenDetailDBModel", foreign_keys=[to_token_id])
     contract_address = Column("contract_address", VARCHAR(250))
     receiving_address = Column("receiving_address", VARCHAR(250))
-    conversion_fee = relationship("ConversionFeeDBModel", foreign_keys=[conversion_fee_id])
+    conversion_fee = relationship(ConversionFeeDBModel, foreign_keys=[conversion_fee_id])
     __table_args__ = (UniqueConstraint(from_token_id, to_token_id), {})
 
 
-class WalletPayerDBModel(Base, AuditClass):
-    __tablename__ = "wallet_payer"
-    id = Column("id", VARCHAR(250), primary_key=True)
-    token_pair_id = Column("token_pair_id", INTEGER, ForeignKey(TokenPairDBModel.id), nullable=False)
+class WalletPairDBModel(Base, AuditClass):
+    __tablename__ = "wallet_pair"
+    wallet_pair_guid = Column("wallet_pair_guid", VARCHAR(250), nullable=False)
+    token_pair_id = Column("token_pair_id", BIGINT, ForeignKey(TokenPairDBModel.id), nullable=False)
     from_address = Column("from_address", VARCHAR(250), nullable=False)
     to_address = Column("to_address", VARCHAR(250))
-    token_pair = relationship('TokenPairDBModel', uselist=False, lazy="select")
+    token_pair = relationship(TokenPairDBModel, uselist=False, lazy="select")
+    __table_args__ = (UniqueConstraint(wallet_pair_guid), {})
 
 
 class ConversionDBModel(Base, AuditClass):
     __tablename__ = "conversion"
-    id = Column("id", VARCHAR(250), primary_key=True)
-    wallet_payer_id = Column("wallet_payer_id", VARCHAR(250), ForeignKey(WalletPayerDBModel.id), nullable=False)
+    conversion_guid = Column("conversion_guid", VARCHAR(250), nullable=False)
+    wallet_pair_id = Column("wallet_pair_id", BIGINT, ForeignKey(WalletPairDBModel.id), nullable=False)
     amount = Column("amount", BIGINT)
     status = Column("status", VARCHAR(30), nullable=False)
-    wallet_address = relationship('WalletAddressDBModel', uselist=False, lazy="select")
-    __table_args__ = (UniqueConstraint(id), {})
+    wallet_pair = relationship(WalletPairDBModel, uselist=False, lazy="select")
+    __table_args__ = (UniqueConstraint(conversion_guid), {})
 
 
 class TransactionDBModel(Base, AuditClass):
     __tablename__ = "transaction"
-    id = Column("id", VARCHAR(250), primary_key=True)
-    conversion_id = Column("conversion_id", VARCHAR(250), ForeignKey(ConversionDBModel.id), nullable=False)
+    transaction_guid = Column("transaction_guid", VARCHAR(250), nullable=False)
+    conversion_id = Column("conversion_id", BIGINT, ForeignKey(ConversionDBModel.id), nullable=False)
     status = Column("status", VARCHAR(30), nullable=False)
     from_transaction_hash = Column("from_transaction_hash", VARCHAR(250))
     from_status = Column("from_status", VARCHAR(30))
@@ -91,5 +93,5 @@ class TransactionDBModel(Base, AuditClass):
     to_transaction_hash = Column("to_transaction_hash", VARCHAR(250))
     to_status = Column("to_status", VARCHAR(30))
     to_updated_at = Column("to_updated_at", TIMESTAMP)
-    conversion = relationship('ConversionDBModel', uselist=False, lazy="select")
-    __table_args__ = (UniqueConstraint(id), {})
+    conversion = relationship(ConversionDBModel, uselist=False, lazy="select")
+    __table_args__ = (UniqueConstraint(transaction_guid), {})
