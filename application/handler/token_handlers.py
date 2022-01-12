b30a741734cdc0f9fb30920b8f74bbcbf2e022e2
karthi96 <karthikeyandon105@gmail.com>
2022-01-12 16:14:48 +0530

    adding python library layers

--- application/handler/token_handlers.py
+++ application/handler/token_handlers.py
@@ -1,9 +1,23 @@
-@exception_handler(SLACK_HOOK=SLACK_HOOK, logger=logger)
+import json
+from http import HTTPStatus
+
+from common.logger import get_logger
+from common.utils import generate_lambda_response, make_response_body
+from config import SLACK_HOOK
+from constants.lambdas import LambdaResponseStatus
+from utils.exception_handler import exception_handler
+from utils.exceptions import EXCEPTIONS
+from utils.lambdas import make_error_format
+
+logger = get_logger(__name__)
+token_service = TokenService()
+
+
+@exception_handler(EXCEPTIONS=EXCEPTIONS, SLACK_HOOK=SLACK_HOOK, logger=logger)
 def get_token_pair(event, context):
-    logger.info(f"Got Airdrops Window Claims Events {event}")
-    return generate_lambda_response(
-        status.value,
-        status.phrase,
-        response,
-        cors_enabled=True,
-    )
+    logger.debug(f"Get all token pair event={json.dumps(event)}")
+    response = token_service.get_token_pair()
+    return generate_lambda_response(HTTPStatus.OK.value,
+                                    make_response_body(status=LambdaResponseStatus.SUCCESS.value, data=response,
+                                                       error=make_error_format()), cors_enabled=True)
+
